50497a60c82b685b079ef833fd688e276a1d855e
tiefengeist <sahirnambiar@gmail.com>
2019-01-20 22:22:48 -0800

    Finished

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+def dictionary
+  #contains the words-to-be-substituted and their substitutes
+
+   dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.size > 140
+    word_substituter(tweet)
+  else tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).size > 140
+    word_substituter(tweet)[0..136] + '...'
+  else tweet
+  end
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
